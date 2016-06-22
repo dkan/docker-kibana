c4492f803ac27552661d6d9972a55226d6bf9252
Damien Kan <damien.kan@gmail.com>
2016-06-22 10:51:31 -0700

    Set proxy port to 80.

--- bin/run-oauthproxy.sh
+++ bin/run-oauthproxy.sh
@@ -1,3 +1,3 @@
 OAUTH2_PROXY_VERSION="2.0.1.linux-amd64.go1.4.2"
 
-exec "/opt/oauth2_proxy" "upstream=${UPSTREAM_URL}"
+exec "/opt/oauth2_proxy" "--upstream=${UPSTREAM_URL}" "--http-address=127.0.0.1:80"
