c69466d2d9cf0aff543c6ffb3af16d10e04d34c6
Damien Kan <damien.kan@gmail.com>
2016-06-22 10:30:27 -0700

    Fix shell syntax.

--- bin/run-oauthproxy.sh
+++ bin/run-oauthproxy.sh
@@ -1,4 +1,3 @@
 OAUTH2_PROXY_VERSION="2.0.1.linux-amd64.go1.4.2"
-UPSTREAM_URL=ENV['UPSTREAM_URL']
 
-exec "/opt/oauth2_proxy -upstream ${UPSTREAM_URL}"
+exec "/opt/oauth2_proxy" "upstream=${UPSTREAM_URL}"
